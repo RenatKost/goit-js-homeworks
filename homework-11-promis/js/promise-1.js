1e96992cfad99eee2a9dac717d6aa1b7673d8b54
RenatKost <renya.show@gmail.com>
2020-02-19 20:49:36 +0400

    hw1

---
+++ homework-11-promis/js/promise-1.js
@@ -0,0 +1,20 @@
+const taskBtn = document.querySelector("[data-action=task-1]")
+
+
+
+const delay = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+taskBtn.addEventListener("click", () => {
+    // Вызовы функции для проверки
+    delay(2000).then(logger); // Resolved after 2000ms
+    delay(1000).then(logger); // Resolved after 1000ms
+    delay(1500).then(logger); // Resolved after 1500ms
+});
---
+++ homework-11-promis/js/promise-2.js
@@ -0,0 +1,40 @@
+const taskBtn = document.querySelector("[data-action=task-2]")
+
+
+
+const users = [{
+        name: "Mango",
+        active: true
+    },
+    {
+        name: "Poly",
+        active: false
+    },
+    {
+        name: "Ajax",
+        active: true
+    },
+    {
+        name: "Lux",
+        active: false
+    }
+];
+
+const toggleUserState = (allUsers, userName) => {
+    return new Promise(resolve => {
+        const updatedUsers = allUsers.map(user =>
+            user.name === userName ? {
+                ...user,
+                active: !user.active
+            } : user
+        );
+        resolve(updatedUsers);
+    });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+taskBtn.addEventListener("click", () => {
+    toggleUserState(users, "Mango").then(logger);
+    toggleUserState(users, "Lux").then(logger);
+});
